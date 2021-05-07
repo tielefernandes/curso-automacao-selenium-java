0effd9d47242aab33048a1f0046a6bee125cbf42
Tiele Fernandes <30751827+tielefernandes@users.noreply.github.com>
2021-05-07 17:33:53 -0300

    Usando @Before e @After do JUnit - Apêndice

--- webdriver-java/src/test/java/tests/informacoesUsuarioTest.java
+++ webdriver-java/src/test/java/tests/informacoesUsuarioTest.java
@@ -1,22 +1,33 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class informacoesUsuarioTest {
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+    private WebDriver navegador;
+
+    @Before
+    public void setUp(){
         // Abrindo o navegador
         System.setProperty("webdriver.chrome.driver", "C:\\cursos\\automacao_api\\drivers\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         // Navegando para a página do Tasklist!
         navegador.get("http://juliodelima.com.br/taskit");
+    }
 
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
         // Clicar no link que possui texto "Sign in"
         navegador.findElement(By.linkText("Sign in")).click();
 
@@ -33,7 +44,14 @@ public class informacoesUsuarioTest {
         navegador.findElement(By.linkText("SIGN IN")).click();
 
         // Validar que dentro do elemento com class "me" está o texto "Hi, Tiele"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals( "Hi, Tiele", textoNoElementoMe);
+    }
 
+    @After
+    public void tearDown(){
         // Fechar o navegador
+        navegador.quit();
     }
 }
